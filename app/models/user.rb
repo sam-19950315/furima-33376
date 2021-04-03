3bb3318d7e08000551c0a561fb976dd940578f38
sam-19950315 <isamucap@gmail.com>
2021-04-03 21:04:46 +0900

    with_optionsの再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
     validates :nickname
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角ひらがな、全角カタカナ、全角漢字で記入してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角ひらがな、全角カタカナ、全角漢字で記入してください' }
-    validates :last_name_katakana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナで記入してください' }
-    validates :first_name_katakana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナで記入してください' }
     validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角ひらがな、全角カタカナ、全角漢字で記入してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence:true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナで記入してください' } do
+    validates :last_name_katakana
+    validates :first_name_katakana
   end
 end
