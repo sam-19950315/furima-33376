9a5e439f0ffff41b035914cecd408b0a3541a756
sam-19950315 <isamucap@gmail.com>
2021-04-05 21:25:59 +0900

    コントローラー（new,create）の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :product_explanation, :product_category_id, :product_quality_id, :shipping_charges_id, :prefecture_id, :delivery_days_id, :price, :user)
+  end
 end
