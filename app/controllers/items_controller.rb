5fce873ff66ae427e147147390cd60026d78b943
sam-19950315 <isamucap@gmail.com>
2021-04-14 12:39:06 +0900

    可読性を高めるための修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :find, only: [:show, :edit, :update]
+  before_action :security, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -10,16 +12,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -42,5 +40,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :product_explanation, :product_category_id, :product_quality_id,
                                  :shipping_charges_id, :prefecture_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def find
+    @item = Item.find(params[:id])
+  end
+
+  def security
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
