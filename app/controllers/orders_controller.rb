f05195c02f1bef06c979693aa0bd3933a3f860c0
sam-19950315 <isamucap@gmail.com>
2021-04-27 23:09:26 +0900

    クレジットカード機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,10 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_purchase = OrderPurchase.new(order_params)
       if @order_purchase.valid?
+        pay_item
         @order_purchase.save
         redirect_to root_path
       else
@@ -23,7 +25,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_purchase).permit(:postal_code, :prefecture_id, :manicipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_purchase).permit(:postal_code, :prefecture_id, :manicipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 
 end
--- app/models/order_purchase.rb
+++ app/models/order_purchase.rb
@@ -1,6 +1,6 @@
 class OrderPurchase
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :manicipality, :address, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :manicipality, :address, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with:  /\A[0-9]{3}-[0-9]{4}\z/, message: "は、3桁数字-4桁数字のハイフンありで記載してください。" }
@@ -8,11 +8,12 @@ class OrderPurchase
     validates :manicipality
     validates :address
     validates :phone_number, format: { with:  /\A\d{10}$|^\d{11}\z/, message: "は、-なしの10桁か11桁で記入してください。"}
+    validates :token
   end
 
   def save
     purchase  =  Purchase.create(item_id: item_id, user_id: user_id)
-    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, manicipality: manicipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, manicipality: manicipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id, token: token)
   end
 
 end
