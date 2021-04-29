85cc02f0668b904069e8a5f136026fb19a7095c1
sam-19950315 <isamucap@gmail.com>
2021-04-29 13:05:47 +0900

    バリデーションの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :find, only: [:index, :create]
 
   def index
