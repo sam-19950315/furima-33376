6e55564a50e1486cf537eb3f2eac846751c6d827
sam-19950315 <isamucap@gmail.com>
2021-04-29 12:10:02 +0900

    endの数修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,11 +16,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    end 
   end
 
   def update
-    end 
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,8 +43,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:product_name, :product_explanation, :product_category_id, :product_quality_id,
-                                 :shipping_charges_id, :prefecture_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :product_explanation, :product_category_id, :product_quality_id, :shipping_charges_id, :prefecture_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def find
@@ -60,6 +57,7 @@ class ItemsController < ApplicationController
   def purchase_exist
     if Purchase.exists?(item_id: @item.id)
       redirect_to root_path
+    end
   end
 
 end
