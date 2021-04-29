4e28a7c83ce7bbe6152b0bd69557511dcc6d9145
sam-19950315 <isamucap@gmail.com>
2021-04-29 13:06:16 +0900

    コメントの削除

--- spec/factories/order_purchases.rb
+++ spec/factories/order_purchases.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     manicipality    { Faker::Name.initials(number: 10) }
     address     { Faker::Name.initials(number: 10) }
     building_name    { Faker::Name.initials(number: 10) }
-    # phone_number          { Faker::Number.number(digits: 11) }
     phone_number          { "09011111111" }
     token          {"tok_abcdefghijk000000000000000"}
   end
